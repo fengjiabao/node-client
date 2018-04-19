62c78a31d46cf15b03903758f7b5fc05921dba3f
fengjb <18794758986@163.com>
2018-04-19 15:49:33 +0800

    build pro

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <h2 @click="postData">Essential Links</h2>
+    
+  </div>
+</template>
+
+<script>
+import qs from 'qs'
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+    postData: function() {
+      let data ={name:'fjb',pwd: '123'}
+      console.log('data',data)
+      this.$http({
+        method: "post",
+        url: "http://127.0.0.1:8888/process_post",
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },//'application/json'       
+        data: qs.stringify(data)
+      
+      }).then(function (response) {
+        console.log(response);
+      })
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
