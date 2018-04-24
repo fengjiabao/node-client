612ce67823d09603306fdfac06c3a7c20e08e07c
fengjb <18794758986@163.com>
2018-04-24 18:05:35 +0800

    test

--- src/components/page-login.vue
+++ src/components/page-login.vue
@@ -1,16 +1,17 @@
 <template>
     <div>
       <article v-if="!showRegisterTips">
-        <p>
+        <!-- <p>
           <span>用户名:</span><input type="text" v-model="user"/>
         </p>
         <p>
           <span>密码:</span><input type="password" v-model="password"/>
-        </p>
+        </p> -->
         <p> 
           <h1>{{ tips }}</h1>
           <button @click="dologin">登录</button>
-          <p @click="doregister">还没帐户？马上注册。</p>
+          <h2 @click="download">下载</h2>
+          <!-- <p @click="doregister">还没帐户？马上注册。</p> -->
         </p>
       </article>
       <page-register v-if="showRegisterTips"></page-register>
@@ -47,6 +48,25 @@ export default {
     },
     doregister: function() {
       this.showRegisterTips = true
+    },
+    download: function (params) {
+       this.$http({
+        method: 'post',
+        url: '/downloadfile',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' } // 'application/json'
+      }).then((response) => {
+        console.log(response.data)
+         let link = document.createElement('a')
+          let absoluteUrl = 'http:127.0.0.1:8888/expTest.xlsx'
+          link.setAttribute('href', absoluteUrl)
+          link.setAttribute('target', '_blank')
+          link.setAttribute('download', 'expTest.xlsx')
+          // link.click()
+          window.setTimeout(function () { // 标签还未创建完成或者服务器文件还未生成！
+            // console.log('Got report response. dataset : ', ds)
+            link.click()
+          }, 2000)
+      })
     }
   }
 }
